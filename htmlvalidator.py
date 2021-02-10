d218ea72dec9e4c1c0baf20313f404e4daada259
Ajaydip Singh <singh@minerva.kgi.edu>
2021-02-11 00:44:00 +0300

    :sparkles: feat: get errors from validator

--- htmlvalidator.py
+++ htmlvalidator.py
@@ -10,13 +10,12 @@ browser.get('https://validator.w3.org/#validate_by_input')
 
 delay = 10
 
-
 try: 
     input_area = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID,"fragment"))) 
 except TimeoutException:
     print('Failed to find input area')
 
-input_area.send_keys('Some random text')
+input_area.send_keys('Random Text')
 
 try: 
     check_btn = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="validate-by-input"]/form/p[2]/a')))
@@ -25,9 +24,11 @@ except TimeoutException:
 
 check_btn.click()
 
-
 try: 
-    results = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID,"results"))) 
-except TimeoutException:
+    results_elem = WebDriverWait(browser, delay).until(EC.presence_of_all_elements_located((By.XPATH,'//*[@id="results"]'))) 
+except:
     print('Failed to find results')
 
+errors = []
+for i in range(len(results_elem)):
+    errors.append(results_elem[i].text) 
