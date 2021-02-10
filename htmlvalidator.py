8ff7e267bfc7deff27fd7c58b116c29cca470481
Ajaydip Singh <singh@minerva.kgi.edu>
2021-02-11 01:37:22 +0300

    :sparkles: feat: add run from terminal logic

--- htmlvalidator.py
+++ htmlvalidator.py
@@ -1,19 +1,29 @@
+import sys
+import subprocess
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
+filename = sys.argv[1]
+with open(filename, 'r') as f:
+    content = f.read()
 
+chrome_options = Options()
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--headless")
 
-browser = webdriver.Chrome()
+browser = webdriver.Chrome(options=chrome_options)
 browser.get('https://validator.w3.org/#validate_by_input')
 
 delay = 10
 
 try: 
     input_area = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID,"fragment"))) 
-    input_area.send_keys('Random Text')
+    input_area.send_keys(content)
     
     check_btn = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="validate-by-input"]/form/p[2]/a')))
     check_btn.click()
@@ -24,10 +34,10 @@ try:
         for i in range(len(results_elem)):
             f.write(results_elem[i].text) 
 
-
 except Exception as e:
     print(e)
 
 finally:
     browser.quit()
+    
 
