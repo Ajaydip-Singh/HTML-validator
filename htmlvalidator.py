869c0c4d787b86638297dee53bbbf53fd14fd813
Ajaydip Singh <singh@minerva.kgi.edu>
2021-02-10 23:55:09 +0300

    :sparkles: feat: add submit functionality

--- htmlvalidator.py
+++ htmlvalidator.py
@@ -5,5 +5,8 @@ browser = webdriver.Chrome()
 browser.get('https://validator.w3.org/#validate_by_input')
 
 input_area = browser.find_element_by_id('fragment')
+check_btn = browser.find_element_by_xpath('//*[@id="validate-by-input"]/form/p[2]/a')
 
 input_area.send_keys('Some random text')
+
+check_btn.click()
