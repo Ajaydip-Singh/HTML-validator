dd5c41580f74b5b89a04d9fb1d9933874735b1b6
Ajaydip Singh <singh@minerva.kgi.edu>
2021-02-11 00:13:41 +0300

    :sparkles: feat: wait before finding element

--- htmlvalidator.py
+++ htmlvalidator.py
@@ -1,12 +1,26 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 browser = webdriver.Chrome()
 
 browser.get('https://validator.w3.org/#validate_by_input')
 
-input_area = browser.find_element_by_id('fragment')
-check_btn = browser.find_element_by_xpath('//*[@id="validate-by-input"]/form/p[2]/a')
+delay = 10
+
+try: 
+    input_area = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID,"fragment"))) 
+except TimeoutException:
+    print('Failed to find input area')
 
 input_area.send_keys('Some random text')
+
+try: 
+    check_btn = browser.find_element_by_xpath('//*[@id="validate-by-input"]/form/p[2]/a')
+except TimeoutException:
+    print('Failed to find submit button')
+
 
 check_btn.click()
