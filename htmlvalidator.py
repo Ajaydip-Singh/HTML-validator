795fa39a01313d00aa1f5375f15e6f69bca6c893
Ajaydip Singh <singh@minerva.kgi.edu>
2021-02-11 01:23:19 +0300

    :sparkles: feat: add errors in file

--- htmlvalidator.py
+++ htmlvalidator.py
@@ -4,8 +4,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
-browser = webdriver.Chrome()
 
+
+browser = webdriver.Chrome()
 browser.get('https://validator.w3.org/#validate_by_input')
 
 delay = 10
@@ -19,9 +20,10 @@ try:
 
     results_elem = WebDriverWait(browser, delay).until(EC.presence_of_all_elements_located((By.XPATH,'//*[@id="results"]'))) 
 
-    errors = []
-    for i in range(len(results_elem)):
-        errors.append(results_elem[i].text) 
+    with open('validatorResult.txt', 'w') as f:
+        for i in range(len(results_elem)):
+            f.write(results_elem[i].text) 
+
 
 except Exception as e:
     print(e)
